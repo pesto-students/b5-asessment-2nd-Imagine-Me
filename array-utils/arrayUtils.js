efdf7614f99b388c307a2475c9a2dcea11a45b18
Prince <prince100thomas@gmail.com>
2020-12-19 10:28:02 +0530

    Added test case for map function

--- array-utils/arrayUtils.js
+++ array-utils/arrayUtils.js
@@ -1,7 +1,13 @@
-
+const map = function (func) {
+  const result = []
+  for (let i = 0; i < this.length; i++) {
+    result.push(func(this[i], i, this))
+  }
+  return result
+}
 export {
-  forEach,
-  map,
-  filter,
-  reduce,
+  // forEach,
+  map
+  // filter,
+  // reduce,
 }
--- array-utils/arrayUtils.test.js
+++ array-utils/arrayUtils.test.js
@@ -1,1 +1,13 @@
-// Write your own test cases.
+import { map } from './arrayUtils'
+Array.prototype.mymap = map
+
+describe('test for map function', () => {
+    test('should return same array', () => {
+        expect([1, 2, 3].mymap(v => v)).toEqual(expect.arrayContaining([1, 2, 3]))
+    })
+
+    test('should return double the array array', () => {
+        expect([1, 2, 3].mymap(v => v * 2)).toEqual(expect.arrayContaining([2, 4, 6]))
+    })
+
+})
